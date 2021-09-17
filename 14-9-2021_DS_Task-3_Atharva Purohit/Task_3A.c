f6fcc50f1f3f619623f276942704945299ad3960
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-09-17 13:30:03 +0530

    third task

---
+++ 14-9-2021_DS_Task-3_Atharva Purohit/Task_3A.c
@@ -0,0 +1,138 @@
+// Write a code to check if a given set of character array consisting of {,},(,),[,] is balanced or not.
+// Test Cases:
+// Input 1: []{([])} Output: Yes
+// Input 2: {(}{)} Output: No
+// Input 3: {[]})( Output: No
+// Input 4: {[()]} Output: Yes
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 20
+
+// Structure for stack
+struct stack {
+  char st[MAX];
+  int top;
+}s;
+
+// Initializing top for stack
+void init(){
+    s.top = -1;
+}
+
+// Check if stack is full
+int isFull(){
+    if(s.top == (MAX-1)){
+        return 1;
+    }
+    return 0;
+}
+
+// Check if stack is empty
+char isEmpty(){
+    if(s.top == -1){
+        return 1;
+    }
+    return 0;
+}
+
+// Push Element
+void push(char data){
+    if(isFull(s)){
+        printf("[!!] Stack Overflow \n");
+        exit(0);
+    }else{
+        s.st[++(s.top)] = data;
+        return;
+    }
+}
+
+// Pop and return top element
+char pop(){
+    if(isEmpty(s)){
+        printf("[!!] Stack Underflow \n");
+        exit(1);
+    }else{
+        int x;
+        x = s.st[s.top];
+        (s.top)--;
+        return x;
+    }
+}
+
+// Return top element
+char topElement(){
+    if(isEmpty(s)){
+        printf("[!!] Stack Underflow \n");
+        return;
+    }
+    return s.st[s.top];
+}
+
+// Function to print stack
+void printStack(){
+    if(!isEmpty(s)){
+        for(int i=(s.top); i>=0; i--){
+            printf("%d \n", (s.st[i]));
+        }
+    }
+}
+
+// Function to check if the character array is balanced or not
+// Return 1 if it is and 0 if it is not
+int isBalanced(char exp[]){
+    // write your code here
+    char A[3]={'{','[','('};
+    char B[3]={'}',']',')'};
+
+
+    for(int i=0;i<strlen(exp);i++)
+    {
+        for(int j=0;j<3;j++)
+         if(exp[i]==A[j])
+            {
+                push(exp[i]);
+            }
+
+        for(int k=0;k<3;k++)
+        {
+            if(exp[i]==B[k])
+            {
+                if(A[k]==topElement())
+                {
+                    pop();
+                }
+
+                else{
+                printf("\nA\n");
+                return 0;
+            }
+            }
+        }
+
+
+    }
+
+    if(isEmpty())
+    {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+
+}
+
+// Driver Code
+int main() {
+
+    init();
+
+    char exp[MAX];
+    printf(">> Expression: \n");
+    scanf("%s", exp);
+    isBalanced(exp) ? printf("Yes \n") : printf("No \n");
+
+  return 0;
+}
